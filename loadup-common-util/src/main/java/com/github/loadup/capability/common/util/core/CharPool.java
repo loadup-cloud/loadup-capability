734810c245327f3283bd1e388971b784088013bf
Laysan <lslvxy@gmail.com>
2023-07-05 17:38:31 +0800

    初始化代码

---
+++ loadup-common-util/src/main/java/com/github/loadup/capability/common/util/core/CharPool.java
@@ -0,0 +1,82 @@
+package com.github.loadup.capability.common.util.core;
+
+/*-
+ * #%L
+ * loadup-common-util
+ * %%
+ * Copyright (C) 2022 - 2023 loadup_cloud
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+/**
+ * char 常量池
+ *
+ * @author L.cm
+ */
+public interface CharPool {
+    // @formatter:off
+    char UPPER_A          = 'A';
+    char LOWER_A          = 'a';
+    char UPPER_Z          = 'Z';
+    char LOWER_Z          = 'z';
+    char DOT              = '.';
+    char AT               = '@';
+    char LEFT_BRACE       = '{';
+    char RIGHT_BRACE      = '}';
+    char LEFT_BRACKET     = '(';
+    char RIGHT_BRACKET    = ')';
+    char DASH             = '-';
+    char PERCENT          = '%';
+    char PIPE             = '|';
+    char PLUS             = '+';
+    char QUESTION_MARK    = '?';
+    char EXCLAMATION_MARK = '!';
+    char EQUALS           = '=';
+    char AMPERSAND        = '&';
+    char ASTERISK         = '*';
+    char STAR             = ASTERISK;
+    char BACK_SLASH       = '\\';
+    char COLON            = ':';
+    char COMMA            = ',';
+    char DOLLAR           = '$';
+    char SLASH            = '/';
+    char HASH             = '#';
+    char HAT              = '^';
+    char LEFT_CHEV        = '<';
+    char NEWLINE          = '\n';
+    char N                = 'n';
+    char Y                = 'y';
+    char QUOTE            = '\"';
+    char RETURN           = '\r';
+    char TAB              = '\t';
+    char RIGHT_CHEV       = '>';
+    char SEMICOLON        = ';';
+    char SINGLE_QUOTE     = '\'';
+    char BACKTICK         = '`';
+    char SPACE            = ' ';
+    char TILDA            = '~';
+    char LEFT_SQ_BRACKET  = '[';
+    char RIGHT_SQ_BRACKET = ']';
+    char UNDERSCORE       = '_';
+    char ONE              = '1';
+    char ZERO             = '0';
+    // @formatter:on
+}
---
+++ loadup-common-util/src/main/java/com/github/loadup/capability/common/util/core/StringPool.java
@@ -0,0 +1,95 @@
+package com.github.loadup.capability.common.util.core;
+
+/*-
+ * #%L
+ * loadup-common-util
+ * %%
+ * Copyright (C) 2022 - 2023 loadup_cloud
+ * %%
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * #L%
+ */
+
+/**
+ * 静态 String 池
+ *
+ * @author L.cm
+ */
+public interface StringPool {
+    // @formatter:off
+    String AMPERSAND         = "&";
+    String AND               = "and";
+    String AT                = "@";
+    String ASTERISK          = "*";
+    String STAR              = ASTERISK;
+    String BACK_SLASH        = "\\";
+    String COLON             = ":";
+    String COMMA             = ",";
+    String DASH              = "-";
+    String DOLLAR            = "$";
+    String DOT               = ".";
+    String EMPTY             = "";
+    String EQUALS            = "=";
+    String FALSE             = "false";
+    String SLASH             = "/";
+    String HASH              = "#";
+    String HAT               = "^";
+    String LEFT_BRACE        = "{";
+    String LEFT_BRACKET      = "(";
+    String LEFT_CHEV         = "<";
+    String NEWLINE           = "\n";
+    String N                 = "n";
+    String NO                = "no";
+    String NULL              = "null";
+    String OFF               = "off";
+    String ON                = "on";
+    String PERCENT           = "%";
+    String PIPE              = "|";
+    String PLUS              = "+";
+    String QUESTION_MARK     = "?";
+    String EXCLAMATION_MARK  = "!";
+    String QUOTE             = "\"";
+    String RETURN            = "\r";
+    String TAB               = "\t";
+    String RIGHT_BRACE       = "}";
+    String RIGHT_BRACKET     = ")";
+    String RIGHT_CHEV        = ">";
+    String SEMICOLON         = ";";
+    String SINGLE_QUOTE      = "'";
+    String BACKTICK          = "`";
+    String SPACE             = " ";
+    String TILDA             = "~";
+    String LEFT_SQ_BRACKET   = "[";
+    String RIGHT_SQ_BRACKET  = "]";
+    String TRUE              = "true";
+    String UNDERSCORE        = "_";
+    String UTF_8             = "UTF-8";
+    String GBK               = "GBK";
+    String Y                 = "y";
+    String YES               = "yes";
+    String ONE               = "1";
+    String ZERO              = "0";
+    String DOLLAR_LEFT_BRACE = "${";
+    String GET               = "get";
+    String IS                = "is";
+    String UNKNOWN           = "unknown";
+    String OK                = "ok";
+
+    // @formatter:on
+}
